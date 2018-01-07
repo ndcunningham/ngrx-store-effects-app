98cff15719d73b987afedabd73aebafa33d3a523
ncunningham <ndcunningham@gmail.com>
2018-01-06 20:01:04 -0500

    Router state serialization and ngrx router

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,4 +1,8 @@
-import { Params } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Params
+} from '@angular/router';
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
 
 import * as fromRouter from '@ngrx/router-store';
@@ -20,3 +24,21 @@ export const reducers: ActionReducerMap<State> = {
 export const getRouterState = createFeatureSelector<
   fromRouter.RouterReducerState<RouterStateUrl>
 >('routerReducer');
+
+export class CustomerSerializer
+  implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    // destructure only that property which we need
+    const { url } = routerState;
+    const { queryParams } = routerState.root;
+
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      state = state.firstChild;
+    }
+
+    const { params } = state;
+
+    return { url, queryParams, params };
+  }
+}
