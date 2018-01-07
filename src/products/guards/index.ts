215761f02e780bb5a668ead0542e81312f49d590
ncunningham <ndcunningham@gmail.com>
2018-01-07 14:42:33 -0500

    Check if pizza exists using a guard

--- src/products/guards/index.ts
+++ src/products/guards/index.ts
@@ -1,5 +1,7 @@
 import { PizzaGuard } from './pizza.guard';
+import { PizzaExistsGuard } from './pizza-exists.guard';
 
-export const guards: any[] = [PizzaGuard];
+export const guards: any[] = [PizzaGuard, PizzaExistsGuard];
 
 export * from './pizza.guard';
+export * from './pizza-exists.guard';
